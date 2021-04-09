a9d142ea1fb208c493981a028a2930094fa516a5
Jose G. Cabrera <72307972+Jose-G-Cabrera@users.noreply.github.com>
2021-04-08 22:09:06 -0500

    Initial commit of heatmap and csv file using plotly

---
+++ Heatmap.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import plotly.express as pe
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash. dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+df = pd.read_csv("States.csv")
+print (df[:5])
+
+app.layout = html.Div([
+
+    dcc.Dropdown(id="year",
+        options=[
+            {"label": "2021", "value":2021}],
+        value=2021,
+        ),
+
+    dcc.Graph(id='us_heatmap', figure={})
+
+])
+
+@app.callback(
+        Output(component_id='us_heatmap',component_property='figure'),
+        [Input(component_id='year',component_property='value')]
+    )
+def graph(current_year):
+
+    fig = pe.choropleth(
+            data_frame=df,
+            locationmode='USA-states',
+            locations='State_code',
+            scope='usa',
+            color= 'Deaths',
+            hover_data=['State','Deaths'],
+            color_continuous_scale=pe.colors.sequential.Aggrnyl[::-1],
+            labels={'Deaths': 'Cumulative Number of Deaths as of Apr 6 2021'},
+            template='seaborn'
+            #template='plotly_dark'
+        )
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
